d4dfe1818103b012d6acad7ebe89152ae449fe61
Joshua Ko <jko11@terpmail.umd.edu>
2021-05-05 10:32:20 -0400

    test script framework

---
+++ Test_Final_Project_Life_Expectancy.py
@@ -0,0 +1,65 @@
+import Final_Project_Life_Expectancy as proj
+import pytest
+
+#Create the class
+class dframe:
+    
+    def __init__():
+        
+    
+    def read_dframe():
+    
+    
+    def check_input():
+
+        
+    def year():
+
+
+    def polio():
+        
+ 
+    def total_exp():
+
+    
+    def schooling():
+
+
+    def income_comp():
+
+ 
+    def alcohol():
+
+        
+    def diphtheria():
+
+
+    def adult_mortality():
+
+
+    def population():
+
+
+    def calc_coefficient():
+
+        
+
+ 
+#call the class
+def main(filepath):     
+    execute = dframe(filepath, user_input=0)
+    execute.read_dframe()
+    execute.check_input()
+    execute.year()
+    execute.polio()
+    execute.income_comp()
+    execute.schooling()
+    execute.total_exp()
+    execute.alcohol()
+    execute.diphtheria()
+    execute.adult_mortality()
+    execute.population()
+    execute.calc_coefficient()
+
+if __name__ == "__main__":
+    main(sys.argv[1])
