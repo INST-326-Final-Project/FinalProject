34eb51de0dac3a07be6f677898b8535f8cf7e9fa
Leek <mccallah4@terpmail.umd.edu>
2021-05-07 19:20:13 -0400

    Added graph_list & deleted other factors

--- Final_Project_Life_Expectancy.py
+++ Final_Project_Life_Expectancy.py
@@ -88,180 +88,21 @@ class dframe:
             final_title_output = " ".join(final_title)
             self.user_input = final_title_output
 
-        
-
-    def year(self):
-        """Display a graph of a country's year factor influencing life 
-        expectancy. 
+    def graph(self, graph):
+        """Display a graph of a country's year factor influencing life expectancy. 
         Years range from 2000 to 2015 for each country.  
-    
+        
         Args:
             user_input(str): country name the user inputs.
-        
+            
         Returns:
             A graph of the regression betweeen year and life expectency.
-    
-        Side effects:
-            outputs graph to stdout.
-        """
-        year_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(year_df, x_vars = ["Year"], y_vars = ["Life expectancy "], 
-                     kind = "reg")
-        plt.show()  
-
-    def polio(self):
-        """Display a graph of a country's polio factor influencing 
-        life expectancy.
-        Polio (Pol3) immunization coverage among 1-year-old infants.
-    
-        Args:
-            user_input(str): country name the user inputs.
-    
-        Returns:
-            A graph of the regression betweeen polio immunization and life 
-            expectency.
         
         Side effects:
             outputs graph to stdout.
         """
-        polio_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(polio_df, x_vars = ["Polio"], y_vars = ["Life expectancy "],
-                     kind = "reg")
-        plt.show()
-
-    def total_exp(self):
-        """Display a graph of a country's total expenditure factor influencing 
-        life expectancy. General government expenditure on health as a 
-        percentage of total government expenditure. 
-    
-        Args:
-            user_input(str): country name the user inputs.
-    
-        Returns:
-            A graph of the regression betweeen governmnet expenditure and life 
-            expectency.
-        
-        Side effects:
-            outputs graph to stdout.
-        """
-        total_exp_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(total_exp_df, x_vars = ["Total expenditure"], y_vars = 
-                     ["Life expectancy "], kind = "reg")
-        plt.show()
-    
-    def schooling(self):
-        """Display a graph of a country's schooling factor influencing life 
-         expectancy.
-        
-        Args:
-         user_input(string): The country that schooling will be analyzed and 
-            derived from
-    
-        Returns:
-            A graph of the regression betweeen schooling and life expectency.
-        
-        Side effects:
-            outputs graph to stdout.
-        """
-        schooling_df = self.df[self.df["Country"] == self.user_input] 
-        sns.pairplot(schooling_df, x_vars = ["Schooling"], 
-                     y_vars = ["Life expectancy "], kind = "reg")
-        plt.show()
-
-    def income_comp(self):
-        """Display a graph of a country's Income composition of resources factor 
-        influencing life expectancy.
-
-        Args:
-            user_input(string): The country that the income_comp will be derived 
-            from
-    
-        Returns:
-            A graph of the regression between income_comp and life expectency.
-        
-        Side effects:
-            Outputs graph to stdout.
-     """
-        income_comp_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(income_comp_df, x_vars = 
-                     ["Income composition of resources"], y_vars = 
-                     ["Life expectancy "], kind = "reg")
-        plt.show()
-        
-    def alcohol(self):
-        """Display a graph of a country's alcohol consumption factor influencing 
-        life expectancy. Will analyze the affect of this factor on the 
-        independent variable
-    
-        Args:
-            user_input(str): The name of the country that this is being 
-            analyzed on
-    
-        Returns:
-            A graph of the regression between alcohol and life expectency.
-    
-        Side effects:
-            outputs graph to stdout.
-        """ 
-        alcohol_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(alcohol_df, x_vars = ["Alcohol"], y_vars = 
-                     ["Life expectancy "], kind = "reg")
-        plt.show()
-        
-    def diphtheria(self):
-        """Display a graph of a country's diphtheria factor influencing life 
-        expectancy. Shows the factor of Diptheria tatnus toxoid & pertussis 
-        (DTP3) immunization amongst young children (1-year olds).
-            
-        Args:
-            user_input(str): country name the user inputs.
-        
-        Returns:
-            A graph of the regression between diphtheria and life expectency.
-            
-        Side effects:
-                outputs graph to stdout.
-        """ 
-        dip_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(dip_df, x_vars=["Diphtheria "],y_vars=["Life expectancy "], 
-                     kind='reg')
-        plt.show()
-
-    def adult_mortality(self):
-        """Display a graph of a country's adult mortality factor influencing 
-        life expectancy. Adult mortality rates are presented as the probablility 
-        of dying between the ages of 15 to 60 per 1000 population. 
-        
-        Args:
-            user_input(str): country name the user inputs.
-        Returns:
-            A graph of the regression between adult_mortality and life 
-            expectency.
-            
-        Side effects:
-                outputs graph to stdout.
-        """ 
-        am_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(am_df, x_vars=["Adult Mortality"],
-                     y_vars=["Life expectancy "], kind='reg')
-        plt.show()
-    
-    def population(self):
-        """Display a graph of a country's population factor influencing life 
-        expectancy. Populations range within the million to tens of millions. 
-        
-        Args:
-            user_input(str): country name the user inputs.
-        
-        Returns:
-            A graph of the regression between population and life expectency.
-            
-        Side effects:
-                outputs graph to stdout.
-        """
-        pop_df = self.df[self.df["Country"] == self.user_input]
-        sns.pairplot(pop_df, x_vars=["Population"],y_vars=["Life expectancy "], 
-                     kind='reg')
+        graph_df = self.df[self.df["Country"] == self.user_input]  
+        sns.pairplot(graph_df, x_vars=[graph],y_vars=["Life expectancy "], kind='reg')
         plt.show()
         
     def calc_coefficient(self):
@@ -294,18 +135,14 @@ def main(filepath):
     functions outside of the class. 
     
     """
+    graph_list = ["Year","Life expectancy ","Adult Mortality","Alcohol","Polio",
+                  "Total expenditure","Diphtheria ","Population",
+                  "Income composition of resources","Schooling"]
     execute = dframe(filepath)
     execute.read_dframe()
     execute.check_input()
-    execute.year()
-    execute.polio()
-    execute.total_exp()
-    execute.schooling()
-    execute.income_comp()
-    execute.alcohol()
-    execute.diphtheria()
-    execute.adult_mortality()
-    execute.population()
+    for a in graph_list:
+         execute.graph(a)
     execute.calc_coefficient()
     
 if __name__ == "__main__":
